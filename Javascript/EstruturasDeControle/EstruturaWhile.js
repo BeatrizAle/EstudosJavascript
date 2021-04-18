7fd76737ea789a17520536ab77d2594496d23585
Beatriz Alencar <beatrizalencar234@gmail.com>
2021-04-17 21:33:23 -0300

    Adição de conteudo de estudos Javascript

--- Javascript/EstruturasDeControle/EstruturaWhile.js
+++ Javascript/EstruturasDeControle/EstruturaWhile.js
@@ -1,2 +1,14 @@
 // Mais usada quando não tem definido um valor de repetição
 
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - max) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1
+while (opcao != 1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a proxima!')
